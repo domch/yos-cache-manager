28212bb75fcc74cc7166fdd84d5a6a712ab7434d
Mehmet Dogan <info@intelligentworks.ch>
2021-07-31 09:57:13 +0200

    Update TestUtils.java

--- src/test/java/ch/intelligentworks/lib/caching/TestUtils.java
+++ src/test/java/ch/intelligentworks/lib/caching/TestUtils.java
@@ -1,12 +1,4 @@
-/*
- *
- * This document contains trade secret data which is the property of
- * Swisscom AG. Information contained herein may not be used,
- * copied or disclosed in whole or part except as permitted by written
- * agreement from Swisscom AG.
- *
- * Copyright (C) 2020 Swisscom AG/Switzerland
- */
+
 
 package ch.intelligentworks.lib.caching;
 
